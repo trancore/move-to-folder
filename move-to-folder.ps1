a333867731c384e36ab9ec2e1590fb2fa2cbab51
trancore <ki.program0915@gmail.com>
2024-03-03 22:54:46 +0900

    feature: update in case that new file

--- move-to-folder.ps1
+++ move-to-folder.ps1
@@ -1,12 +1,13 @@
 Set-PSDebug -strict
+
+# 保管ディレクトリパス（＝カレントディレクトリ）
+$databaseDirectoryPath = $PSScriptRoot
 Set-Location $databaseDirectoryPath
 
 # 移動したいファイルが入ったディレクトリ
 $directoryToBeMoved = ""
 # ファイルの区切り文字
 $delimiter = ""
-# 保管ディレクトリパス（＝カレントディレクトリ）
-$databaseDirectoryPath = $PSScriptRoot
 # 移動したいファイルが入ったディレクトリパス
 $directoryPathToBeMoved = Join-Path $databaseDirectoryPath $directoryToBeMoved
 # ログファイルのファイルパス
@@ -18,6 +19,7 @@ $fileNameListToBeMoved = Get-ChildItem -Path $directoryPathToBeMoved -Name -File
 $databaseDirectoryNameList = Get-ChildItem -Path $databaseDirectoryPath -Name -Directory
 
 foreach ($fileName in $fileNameListToBeMoved) {
+    $fileNameHasDelimiterList = $fileName.Split($delimiter)
     $fileNameNotHasDelimiter = $fileName.Split($delimiter)[0]
     $matchedFileName = $databaseDirectoryNameList | Where-Object { $_ -eq $fileNameNotHasDelimiter }
 
@@ -28,9 +30,18 @@ foreach ($fileName in $fileNameListToBeMoved) {
         }
         catch {
             Write-Error "【${Get-Date}】：【ERROR】 ${$_.Exception.Message}"
+        } 
+    }
+    elseif ($fileNameHasDelimiterList.Length -gt 1) {
+        try {
+            New-Item (Join-Path $databaseDirectoryPath $fileNameNotHasDelimiter) -ItemType Directory
+            Move-Item (Join-Path $directoryPathToBeMoved $fileName) (Join-Path $databaseDirectoryPath $fileNameNotHasDelimiter)
         }
-        
+        catch {
+            Write-Error "【${Get-Date}】：【ERROR】 ${$_.Exception.Message}"
+        }
     }    
+
 }
 
 Stop-Transcript
